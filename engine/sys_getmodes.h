b74d5175d636743509edc9018847b285163ab176
ThePixelMoon <yourlocalmoon7@gmail.com>
2025-09-07 16:18:23 +0300

    shit

--- engine/sys_getmodes.h
+++ engine/sys_getmodes.h
@@ -6,7 +6,34 @@
 
 #include "ivideomode.h"
 typedef uint32 ScreenshotHandle;
+#if defined( _WIN32 ) && !defined( _X360 )
+
+#ifdef _WIN32
+// 
+// Prevent tons of unused windows definitions
+//
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#define NOWINRES
+#define NOSERVICE
+#define NOMCX
+#define NOIME
+#if !defined( _X360 )
+#pragma warning(push, 1)
+#pragma warning(disable: 4005)
+#include <windows.h>
+#pragma warning(pop)
+#endif
+#undef PostMessage
+
+#pragma warning( disable: 4800 )	// forcing value to bool 'true' or 'false' (performance warning)
+
+#endif // WIN32
+
+#elif defined(POSIX)
 typedef void *HDC;
+#endif
 
 //-----------------------------------------------------------------------------
 // Purpose: Functionality shared by all video modes
